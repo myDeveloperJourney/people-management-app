62be76ee1399e1ced67979c6fe7ae1e83022bc85
Daniel JS <daniel@mydeveloperjourney.com>
2022-09-21 12:27:51 -0500

    wip: adds form

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,13 +1,64 @@
+import { useState } from 'react';
+
 function Index(props) {
-    if(props.people) {
+
+    const [ newForm, setNewForm ] = useState({
+        name: '',
+        image: '',
+        title: '',
+    });
+
+    const loaded = () => {
         return props.people.map(person => (
             <div className="person" key={person._id}>
                 <h1>{person.name}</h1>
             </div>
         ));
-    }
+    };
+
+    const loading = () => {
+        return <h1>Loading ...</h1>;
+    };
+
+    const handleChange = () => {};
+    const handleSubmit = () => {};
+
+    return (
+        <section>
+            { props.people ? loaded() : loading() }
+            <form>
+                <label>Name:
+                    <input 
+                        type="text"
+                        value={newForm.name} 
+                        onChange={handleChange}
+                        placeholder="Elizabeth II"
+                        name="name"
+                    />
+                </label>
+                <label>Image:
+                    <input 
+                        type="text"
+                        value={newForm.image} 
+                        onChange={handleChange}
+                        placeholder="https://your-person-image.com/file.jpeg"
+                        name="image"
+                    />
+                </label>
+                <label>Title:
+                    <input 
+                        type="text"
+                        value={newForm.title} 
+                        onChange={handleChange}
+                        placeholder="The Queen of England"
+                        name="title"
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </section>
+    )
     
-    return <h1>Loading ...</h1>;
 }
 
 export default Index;
